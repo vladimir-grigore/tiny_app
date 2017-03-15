0c57b74c20c81b86af0e7df6447b4ae0fe0c211e
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-15 01:04:53 +0000

    Add comments

--- express_server.js
+++ express_server.js
@@ -1,55 +1,67 @@
 var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
-var crypto = require('crypto');
+var crypto = require('crypto');  //Used for generating random numbers
 var PORT = process.env.PORT || 8080;
 
+//Enable ejs to use templates in the views folder
 app.set('view engine', 'ejs');
+
+//Use body parser - used in app.post('/urls')
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Generate a random 6 character hex string
 function generateRandomString(){
-  var id = crypto.randomBytes(3).toString('hex');
-  console.log(id)
+  return crypto.randomBytes(3).toString('hex');
 }
-generateRandomString();
 
+//Holds our instances of shortned urls
+//and their reference to their original form
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//index page
 app.get('/', (request, response) => {
   response.send('Hello');
 });
 
+//return the urlDatabase object in JSON format
 app.get('/urls.json', (request, response) => {
   response.json(urlDatabase);
 });
 
+//display the /urls page - will show all items in urlDatabase object
 app.get('/urls', (request, response) => {
   let templateVars = { urls: urlDatabase };
   response.render('urls_index', templateVars);
 });
 
+//post action after submitting the form on /urls/new
 app.post('/urls', (request, response) => {
   console.log(request.body);
   response.send('OK');
 });
 
+//display the /urls/new page
 app.get('/urls/new', (request, response) => {
   response.render('urls_new');
 });
 
+//display details page for a specific key in urlDatabase object
 app.get('/urls/:id', (request, response) => {
   var templateVars = { shortURL: request.params.id };
   templateVars['url'] = urlDatabase[request.params.id];
   response.render('urls_show', templateVars);
 });
 
+//display the /hello page
 app.get('/hello', (request, response) => {
   response.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//open port 8080
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
