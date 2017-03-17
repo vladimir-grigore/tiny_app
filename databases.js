3db0dbbdfbbbc93d20778335dc8cb48bac3f5d5a
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-17 21:06:12 +0000

    Move databases to separate file

---
+++ databases.js
@@ -0,0 +1,20 @@
+//Holds our instances of shortned urls
+//and their reference to their original form
+var urlDatabase = {
+  "firstUserID": {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  }
+};
+
+//Holds user information
+var users = {
+  "firstUserID": {
+    id: "firstUserID",
+    email: "a@a.a",
+    password: "$2a$05$7.i.tolIwhRACirAlePQaegQlnsvpQRp4.GzcQnFO/RQvj9Y.ORN."
+  }
+};
+
+module.exports.users = users;
+module.exports.urlDatabase = urlDatabase;
